d29d55764b3d899e8d65354efa09fd670342e816
LuccaCasalecchi <72542388+LuccaCasalecchi@users.noreply.github.com>
2020-12-07 15:06:26 -0300

    Add files via upload

---
+++ mememaker/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import GlobalStyles from "./styles/global";
+import Home from "./pages/home";
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <Home />
+    </>
+  );
+}
+
+export default App;
